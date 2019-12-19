bd9b17f1f3c354ebf31826117578c942af2faa94
PeterZhangxing <964725349@qq.com>
2019-12-19 23:01:16 +0800

    test inherits

--- gui_test/test_pyqt/f_slot_signal.py
+++ gui_test/test_pyqt/f_slot_signal.py
@@ -16,6 +16,28 @@ class MyWindow(QWidget):
         self.button1.move(10,10)
         self.button1.clicked.connect(self.change_wtitle)
 
+        self.button2 = QPushButton(self)
+        self.button2.setText('push2')
+        self.button2.move(150,10)
+        self.button2.clicked.connect(self.dis_parent)
+
+        self.lable1 = QLabel(self)
+        self.lable1.setText('label1')
+        self.lable1.move(250,10)
+
+        mylabel = QLabel()
+        mylabel.destroyed.connect(self.test_des)
+
+    def test_des(self,p_object):
+        print('删除对象:',p_object)
+
+    def dis_parent(self):
+        for child in self.children():
+            if child.inherits('QPushButton'):
+                print('button:',child)
+            elif child.inherits('QLabel'):
+                print('label:',child)
+
     def change_wtitle(self):
         self.windowTitleChanged.connect(self.ctitle)
         self.setWindowTitle('My')
