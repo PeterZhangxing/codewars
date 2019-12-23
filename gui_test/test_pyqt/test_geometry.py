b71c808824f82af324b13f5771a5107a62ac47c2
PeterZhangxing <964725349@qq.com>
2019-12-23 23:04:54 +0800

    set size

---
+++ gui_test/test_pyqt/test_geometry.py
@@ -0,0 +1,45 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+class MyWindow(QWidget):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        self.resize(300,300)
+        self.setWindowTitle('test geometry')
+
+        self.setMinimumSize(300,300)
+        self.setMaximumSize(600,600)
+
+        self.total_widgets = 9
+        self.col = 3
+
+        self.init_gui()
+
+    def init_gui(self):
+        my_button_width = self.width() // self.col
+        total_rows = (self.total_widgets - 1) // self.col + 1
+        my_button_height = self.height() // total_rows
+        # print(self.width())
+        # print(self.height())
+        # print(self.geometry().width())
+
+        for i in range(self.total_widgets):
+            my_btn = QPushButton(self)
+            my_btn.setText('btn%d'%i)
+            my_btn.setStyleSheet('background-color:grey;border:1px solid yellow;')
+            my_btn_x = i % self.col * my_button_width
+            my_btn_y = i // self.col * my_button_height
+            my_btn.resize(my_button_width,my_button_height)
+            my_btn.move(my_btn_x,my_btn_y)
+
+
+if __name__ == '__main__':
+    import sys
+
+    app = QApplication(sys.argv)
+
+    mywindow = MyWindow()
+    mywindow.show()
+
+    sys.exit(app.exec_())
